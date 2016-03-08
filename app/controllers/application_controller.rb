1f740e4d2945643a7225944fcd02f950f0006754
Dan Brooking <dan@getpicklejar.com>
2016-03-08 15:41:28 -0500

    Fixes for code climate.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     user_recipes_path
   end
 end
